ce7747f5caf740d4d6da90c649916235a47c0b69
Ilyusha-Novoselov <ilyanov56@mail.ru>
2025-03-08 22:41:04 +0700

    Some changes in Web Server

--- Lab_1/src/WebServers.cxx
+++ Lab_1/src/WebServers.cxx
@@ -7,175 +7,220 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <err.h>
+#include <cstring>
 #include <pthread.h>
-#include <string.h>
+#include <iostream>
 
 #include "WebServers.hxx"
 
 namespace parallel {
 namespace {
-struct ResponseData {
-    int myClientFd;
-    const std::string* myResponse;
+char myResponse[] = "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+    "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+    "<style>body { background-color: #111 }"
+    "h1 { font-size:4cm; text-align: center; color: black;"
+    " text-shadow: 0 0 2mm red}</style></head>"
+    "<body><h1>Goodbye, world!</h1></body></html>\r\n";
+
+struct ThreadData
+{
+    int client_fd;
 };
 
-std::string GetPhpVersion()
+void *ThreadJob (void *arg)
 {
-    FILE* aFilePointer = popen("php -r 'echo phpversion();'", "r");
-    if (!aFilePointer) return "Unknown";
-    
-    char aBuffer[128];
-    std::string aPhpVersion;
-    while (fgets(aBuffer, sizeof(aBuffer), aFilePointer) != nullptr) {
-        aPhpVersion += aBuffer;
+    ThreadData* par = (struct ThreadData*)arg;
+    int client_fd = par->client_fd;
+    write (client_fd, myResponse, strlen (myResponse) * sizeof (char));
+    close (client_fd);
+    return NULL;
+}
+
+void *ThreadJobPhp (void *arg)
+{
+    ThreadData* par = (struct ThreadData*)arg;
+    int client_fd = par->client_fd;
+    FILE* pipe = popen ("php -r 'echo phpversion();'", "r");
+    char myResponse [512];
+    char php_version [128];
+    if (fscanf (pipe, "%s", php_version)) 
+    {
+        pclose (pipe);
+        sprintf (myResponse, "HTTP/1.1 200 OK\r\n"
+                  "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+                  "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+                  "<style>body { background-color: #111 }"
+                  "h1 { font-size:4cm; text-align: center; color: black;"
+                  " text-shadow: 0 0 2mm red}</style></head>"
+                  "<body><h1>Goodbye, world!\n PHP: %s</h1></body></html>\r\n", 
+                  php_version);
+        write (client_fd, myResponse, strlen (myResponse) * sizeof (char));
     }
-    pclose(aFilePointer);
-
-    return aPhpVersion;
+    else {
+        perror ("Can't run php.");
+    }
+    close (client_fd);
+    return NULL;
 }
 
-void* HandleClient(void* theArg) {
-    ResponseData* aResponseData = static_cast<ResponseData*>(theArg);
-
-    write(aResponseData->myClientFd, aResponseData->myResponse->c_str(), aResponseData->myResponse->size());
-
-    close(aResponseData->myClientFd);
-
-    delete aResponseData;
-
-    return nullptr;
-}
-
-void RunServer (const char* theResponse, bool includePhpVersion)
+void RunOneThreadServer (bool thePhpVersion)
 {
     int one = 1, client_fd;
     struct sockaddr_in svr_addr, cli_addr;
-    socklen_t sin_len = sizeof(cli_addr);
+    socklen_t sin_len = sizeof (cli_addr);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-    err(1, "can't open socket");
+    int sock = socket (AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        err (1, "can't open socket");
+    }
 
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+    setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 
     int port = 8080;
     svr_addr.sin_family = AF_INET;
     svr_addr.sin_addr.s_addr = INADDR_ANY;
-    svr_addr.sin_port = htons(port);
+    svr_addr.sin_port = htons (port);
 
-    if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
-        close(sock);
-        err(1, "Can't bind");
+    if (bind (sock, (struct sockaddr *) &svr_addr, sizeof (svr_addr)) == -1) {
+        close (sock);
+        err (1, "Can't bind");
     }
 
-    listen(sock, 5);
+    listen (sock, 5);
     while (1) {
-        client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
-        printf("got connection\n");
+        client_fd = accept (sock, (struct sockaddr *) &cli_addr, &sin_len);
+        printf ("got connection\n");
 
         if (client_fd == -1) {
-            perror("Can't accept");
+            perror ("Can't accept");
             continue;
         }
 
-        std::string aResponse (theResponse);
-        if (includePhpVersion) {
-            aResponse += "<p>PHP Version: " + GetPhpVersion() + "</p>";
+        if (thePhpVersion) {
+            FILE* pipe = popen ("php -r 'echo phpversion();'", "r");
+            char myResponse[512];
+            char php_version[128];
+            if (fscanf (pipe, "%s", php_version)) 
+            {
+                pclose (pipe);
+                sprintf (myResponse, "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+                        "<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+                        "<style>body { background-color: #111 }"
+                        "h1 { font-size:4cm; text-align: center; color: black;"
+                        " text-shadow: 0 0 2mm red}</style></head>"
+                        "<body><h1>Goodbye, world!\n PHP: %s</h1></body></html>\r\n", 
+                        php_version);
+                write (client_fd, myResponse, strlen (myResponse) * sizeof (char));
+            } else {
+                perror ("Can't run php.");
+            }
+            close (client_fd);
+        } else {
+            write (client_fd, myResponse, sizeof(myResponse) - 1); /*-1:'\0'*/
+            close (client_fd);
         }
-        aResponse += "</body></html>\r\n";
-
-        write (client_fd, aResponse.c_str(), strlen (aResponse.c_str()) - 1);
-        close(client_fd);
     }
 }
 
-void RunMultithreadingServer(const std::string& theResponse, bool includePhpVersion)
+void RunMultiThreadServer (bool thePhpVersion)
 {
-    int one = 1;
+    int one = 1, client_fd;
     struct sockaddr_in svr_addr, cli_addr;
-    socklen_t sin_len = sizeof(cli_addr);
+    socklen_t sin_len = sizeof (cli_addr);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) err(1, "can't open socket");
+    int sock = socket (AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        err(1, "can't open socket");
+    }
 
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+    setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 
     int port = 8080;
     svr_addr.sin_family = AF_INET;
     svr_addr.sin_addr.s_addr = INADDR_ANY;
-    svr_addr.sin_port = htons(port);
+    svr_addr.sin_port = htons (port);
 
-    if (bind(sock, (struct sockaddr*) &svr_addr, sizeof(svr_addr)) == -1) {
-        close(sock);
+    if (bind (sock, (struct sockaddr*)&svr_addr, sizeof (svr_addr)) == -1) {
+        close (sock);
         err(1, "Can't bind");
     }
 
+    listen (sock, 5);
+    int err;
     pthread_attr_t thread_attr;
-    if (pthread_attr_init(&thread_attr) != 0) {
-        perror("Cannot initialize thread attributes");
-        exit(EXIT_FAILURE);
+    err = pthread_attr_init (&thread_attr);
+    if (err != 0) {
+        perror ("Cannot init attr for thread");
+        exit (-1);
     }
 
-    if (pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED) != 0) {
-        perror("Cannot set detached state");
-        exit(EXIT_FAILURE);
+    err = pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
+    if (err != 0) {
+        perror ("Cannot create deteched thread");
+        exit (-1);
     }
 
-    if (pthread_attr_setstacksize(&thread_attr, 512 * 1024) != 0) {
-        perror("Cannot set stack size");
-        exit(EXIT_FAILURE);
+    err = pthread_attr_setstacksize (&thread_attr, 1 * 1024 * 1024);
+    if (err != 0) {
+        perror ("Cannot create stack for thread");
+        exit (-1);
     }
 
-    listen(sock, 5);
+    while (1)
+    {
+        client_fd = accept (sock, (struct sockaddr*)&cli_addr, &sin_len);
+        printf ("got connection\n");
 
-    while (1) {
-        int client_fd = accept(sock, (struct sockaddr*) &cli_addr, &sin_len);
-        printf("got connection\n");
-
-        if (client_fd == -1) {
-            perror("Can't accept");
+        if (client_fd == -1)
+        {
+            perror ("Can't accept");
             continue;
         }
-
-        std::string aResponse = theResponse;
-        if (includePhpVersion) {
-            aResponse += "<p>PHP Version: " + GetPhpVersion() + "</p>";
-        }
-        aResponse += "</body></html>\r\n";
-
-        ResponseData* aResponseData = new ResponseData;
-        aResponseData->myClientFd = client_fd;
-        aResponseData->myResponse = &aResponse;
 
         pthread_t thread;
-        if (pthread_create(&thread, &thread_attr, HandleClient, aResponseData) != 0) {
-            perror("pthread_create failed");
-            close(client_fd);
-            delete aResponseData;
+        ThreadData *data = new ThreadData {client_fd};
+        int err;
+        if (thePhpVersion) {
+            err = pthread_create(&thread, NULL, ThreadJobPhp, data);
+        } else {
+            err = pthread_create(&thread, NULL, ThreadJob, data);
         }
+
+        if (err != 0) {
+            std::cout << "Cannot create a thread: " << strerror(err) << std::endl;
+            close (client_fd);
+            delete data;
+            continue;
+        }
+
+        pthread_detach (thread);
+        pthread_attr_destroy (&thread_attr);
     }
+    pthread_exit (NULL);
 }
 
 }
 
-void WebServers::OneThreadServer()
+void WebServers::OneThreadServer() 
 {
-    RunServer (myResponse, false);
+    RunOneThreadServer (false);
 }
 
-void WebServers::OneThreadServerWithPhp() 
+void WebServers::OneThreadServerWithPhp()
 {
-    RunServer (myResponse, true);
+    RunOneThreadServer (true);
 }
 
 void WebServers::MultiThreadServer()
 {
-    RunMultithreadingServer (myResponse, false);
+    RunMultiThreadServer (false);
 }
 
 void WebServers::MultiThreadServerWithPhp()
 {
-    RunMultithreadingServer (myResponse, true);
+    RunMultiThreadServer (true);
 }
 
 }
--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,8 @@
-if [ ! -d "build" ]; then
-  mkdir build
+if [ -d "build" ]; then
+  rm -rf build
 fi
 
+mkdir build
 cd build
 
 cmake ..
