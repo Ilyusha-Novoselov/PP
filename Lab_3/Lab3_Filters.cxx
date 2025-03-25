ff170dd039bc8f34b63f1f54cdab9ef9383c6fcb
Ilyusha-Novoselov <ilyanov56@mail.ru>
2025-03-25 21:45:28 +0700

    Some changes in Lab3_Filter.cxx

--- Lab_3/Lab3_Filters.cxx
+++ Lab_3/Lab3_Filters.cxx
@@ -14,8 +14,41 @@
 typedef std::vector <std::vector <double>> Filter;
 typedef std::unordered_map <std::string, Filter> NameFilterMap;
 
-double ApplyFilter (const NameFilterMap& theFilters, 
-                    const size_t theNumOfThreads,
+Filter GenerateBlurKernel (int theSize) {
+    double aValue = 1.0 / (theSize * theSize);
+    Filter aKernel (theSize, std::vector<double>(theSize, aValue));
+    return aKernel;
+}
+
+Filter GenerateSharpKernel(int theSize) {
+    Filter aKernel (theSize, std::vector <double> (theSize, -1));
+    int aCenter = theSize / 2;
+    aKernel [aCenter][aCenter] = theSize * theSize;
+    return aKernel;
+}
+
+Filter GenerateNegativeKernel (int theSize) {
+    Filter aKernel (theSize, std::vector <double> (theSize, -1));
+    int aCenter = theSize / 2;
+    aKernel [aCenter][aCenter] = theSize * theSize - 1;
+    return aKernel;
+}
+
+int SelectKernelSize (int theWidth, int theHeight) {
+    int aMinDim = std::min (theWidth, theHeight);
+    if (aMinDim <= 500) {
+        return 5;
+    }
+    if (aMinDim <= 1500) {
+        return 7;
+    } 
+    if (aMinDim <= 4000) {
+        return 9;
+    }
+    return 11;
+}
+
+double ApplyFilter (const size_t theNumOfThreads,
                     const std::string& theFileName,
                     const std::string& theFilterName,
                     Table& theTable,
@@ -25,36 +58,49 @@ double ApplyFilter (const NameFilterMap& theFilters,
     png::image <png::rgba_pixel> anImage (theFileName);
     int aWidth = anImage.get_width();
     int aHeight = anImage.get_height();
+
+    int aKernelSize = SelectKernelSize (aWidth, aHeight);
+    NameFilterMap aFilters;
+    aFilters ["Blur"] = GenerateBlurKernel (aKernelSize);
+    aFilters ["Sharp"] = GenerateSharpKernel (aKernelSize);
+    aFilters ["Negative"] = GenerateNegativeKernel (aKernelSize);
+
     png::image <png::rgba_pixel> anFilteredImage (aWidth, aHeight);
 
     auto aStart = std::chrono::steady_clock::now();
+    int anOffset = aKernelSize / 2;
 
-    #pragma omp parallel for collapse (2) num_threads (theNumOfThreads)
-    for (int y = 0; y < aHeight; ++y) {
-        for (int x = 0; x < aWidth; ++x) {
-            int r = 0, g = 0, b = 0;
+    #pragma omp parallel num_threads (theNumOfThreads)
+    {
+        #pragma omp for collapse(2) schedule (static)
+        for (int y = 0; y < aHeight; ++y) {
+            for (int x = 0; x < aWidth; ++x) {
+                double r = 0, g = 0, b = 0;
 
-            for (int j = -1; j <= 1; ++j) {
-                for (int i = -1; i <= 1; ++i) {
-                    int xn = x + i, yn = y + j;
-                    png::rgba_pixel aNeighbour = {0, 0, 0, 255};
+                const Filter& aKernel = aFilters [theFilterName];
 
-                    if (xn >= 0 && yn >= 0 && xn < aWidth && yn < aHeight)
-                        aNeighbour = anImage.get_pixel (xn, yn);
+                for (int j = -anOffset; j <= anOffset; ++j) {
+                    for (int i = -anOffset; i <= anOffset; ++i) {
+                        int xn = x + i, yn = y + j;
+                        png::rgba_pixel aNeighbour = {0, 0, 0, 255};
 
-                    double aCoef = theFilters.at (theFilterName) [j + 1][i + 1];
-                    r += aCoef * aNeighbour.red;
-                    g += aCoef * aNeighbour.green;
-                    b += aCoef * aNeighbour.blue;
+                        if (xn >= 0 && yn >= 0 && xn < aWidth && yn < aHeight)
+                            aNeighbour = anImage.get_pixel(xn, yn);
+
+                        double aCoef = aKernel [j + anOffset][i + anOffset];
+                        r += aCoef * aNeighbour.red;
+                        g += aCoef * aNeighbour.green;
+                        b += aCoef * aNeighbour.blue;
+                    }
                 }
+
+                r = std::min (std::max (static_cast <int>(r), 0), 255);
+                g = std::min (std::max (static_cast <int>(g), 0), 255);
+                b = std::min (std::max (static_cast <int>(b), 0), 255);
+
+                png::rgba_pixel pixel (r, g, b, anImage.get_pixel (x, y).alpha);
+                anFilteredImage.set_pixel (x, y, pixel);
             }
-
-            r = std::min (std::max (r, 0), 255);
-            g = std::min (std::max (g, 0), 255);
-            b = std::min (std::max (b, 0), 255);
-
-            png::rgba_pixel pixel (r, g, b, anImage.get_pixel(x, y).alpha);
-            anFilteredImage.set_pixel (x, y, pixel);
         }
     }
 
@@ -67,7 +113,7 @@ double ApplyFilter (const NameFilterMap& theFilters,
     }
 
     double aSpeedup = theNotParallelTime / aTime;
-    double anEfficiency = (theNotParallelTime / aTime) / theNumOfThreads;
+    double anEfficiency = aSpeedup / theNumOfThreads;
     theTable.addRow ({std::to_string (theNumOfThreads),
                       std::to_string (aWidth) + "x" + std::to_string (aHeight),
                       theFilterName,
@@ -79,54 +125,43 @@ double ApplyFilter (const NameFilterMap& theFilters,
 }
 
 int main() {
-    const NameFilterMap aFilters = {
-        {"Negative", {{-1, -1, -1},
-                      {-1,  8, -1},
-                      {-1, -1, -1}}},
-        {"Sharp", {{0, -1,  0},
-                   {-1, 5, -1},
-                   {0, -1,  0}}},
-        {"Blur", {{1.0 / 9., 1.0 / 9., 1.0 / 9.},
-                  {1.0 / 9., 1.0 / 9., 1.0 / 9.},
-                  {1.0 / 9., 1.0 / 9., 1.0 / 9.}}}
-    };
     const std::string anImgDir = std::filesystem::current_path().string() + "/../Lab_3/img/";
-    const std::vector <std::string> anImages = {anImgDir + "360x437.png",
-                                                anImgDir + "1200x1200.png",
-                                                anImgDir + "3000x3000.png",
-                                                anImgDir + "6192x5785.png"};
+    const std::vector <std::string> anImages = {
+        anImgDir + "360x437.png",
+        anImgDir + "1200x1200.png",
+        anImgDir + "3000x3000.png",
+        anImgDir + "6192x5785.png"
+    };
 
     Table aTable;
-    aTable.setHeaders({"Threads", "Image Resolution", "Filter", "Time", "Speedup", "Efficiency"});
+    aTable.setHeaders ({"Threads", "Image Resolution", "Filter", "Time", "Speedup", "Efficiency"});
 
     std::array <std::string, 3> aFiltersName = {"Negative", "Sharp", "Blur"};
 
     std::unordered_map <std::string, double> aNotParallelTime;
     for (const auto& anImage : anImages) {
         for (const auto& aFilterName : aFiltersName) {
-            aNotParallelTime [anImage + aFilterName] = ApplyFilter (aFilters,
-                                                                    1,
-                                                                    anImage,
-                                                                    aFilterName,
-                                                                    aTable,
-                                                                    false);
+            aNotParallelTime [anImage + aFilterName] = ApplyFilter(1,
+                                                                   anImage,
+                                                                   aFilterName,
+                                                                   aTable,
+                                                                   false);
         }
     }
 
     for (const auto& anImage : anImages) {
         for (const auto& aFilterName : aFiltersName) {
             for (size_t aThreads = 2; aThreads <= 16; aThreads *= 2) {
-                ApplyFilter (aFilters,
-                             aThreads,
+                ApplyFilter (aThreads,
                              anImage,
                              aFilterName,
                              aTable,
-                             aThreads == 16 ? true : false,
+                             aThreads == 16,
                              aNotParallelTime [anImage + aFilterName]);
             }
         }
     }
-    
+
     aTable.PrintInTerminal();
     aTable.PrintForWord();
 
