64123d5502602af3a5980bd7d352e3b075a0eb00
Ilyusha-Novoselov <ilyanov56@mail.ru>
2025-04-11 13:39:16 +0700

    Fixed iterations
    First exchange, then calculate internal points, then wait for the exchange to complete, write ghost layers to U, calculate boundary points

--- Lab_4/Lab4_PoissonEquation.cxx
+++ Lab_4/Lab4_PoissonEquation.cxx
@@ -27,7 +27,6 @@ inline double RightSideFunction (double x, double y, double z) {
     return 6.0;
 }
 
-
 int main (int argc, char** argv) {
     MPI_Init (&argc, &argv);
 
@@ -56,117 +55,142 @@ int main (int argc, char** argv) {
     double Cc = 2.0 * (Cx + Cy + Cz);
 
     // Определим локальный размер по Z с учетом "призрачных" слоев
-    size_t aLocalNZ = NZ / aSize + (aRank == 0 || aRank == aSize - 1 ? 1 : 2);
-
-    // 3D массив: U[i][j][k] => U[(i * NY + j) * aLocalNZ + k]
-    std::vector <double> U((NX + 1) * (NY + 1) * aLocalNZ, 0.0);
-    std::vector <double> U_New((NX + 1) * (NY + 1) * aLocalNZ, 0.0);
+    size_t localNZ = NZ / aSize;
+    size_t aLocalNZ = localNZ + 2; // +2 призрачных слоя
 
     // Лямбда-функция для подсчета индекса в линейном массиве для трехмерного пр-ва
     auto ComputeIndex = [&](size_t i, size_t j, size_t k) {
         return (i * (NY + 1) + j) * aLocalNZ + k;
     };
 
+    // 3D массив: U[i][j][k] => U[(i * NY + j) * aLocalNZ + k]
+    std::vector <double> U ((NX + 1) * (NY + 1) * aLocalNZ, 0.0);
+    std::vector <double> U_New = U;
+
     // Инициализация граничных условий и начальных значений
     for (size_t i = 0; i <= NX; ++i) {
         for (size_t j = 0; j <= NY; ++j) {
             for (size_t k = 0; k < aLocalNZ; ++k) {
                 // Определяется какой глобальный индекс соответствует текущему локальному индексу k для данного процесса
-                size_t aGlobalK = (aRank == 0) ? k
-                                : (aRank == aSize - 1) ? NZ - aLocalNZ + k + 1
-                                : k + aRank * (aLocalNZ - 2) - 1;
+                size_t globalK = aRank * localNZ + k - 1;
+                if (globalK >= NZ) {
+                    continue;
+                }
 
                 double x = MINX + i * Hx;
                 double y = MINY + j * Hy;
-                double z = MINZ + aGlobalK * Hz;
+                double z = MINZ + globalK * Hz;
 
                 // Проверка на граничное условие
-                if (i == 0 || i == NX || j == 0 || j == NY || aGlobalK == 0 || aGlobalK == NZ) {
-                    U[ComputeIndex(i,j,k)] = U_New[ComputeIndex(i,j,k)] = Phi (x, y, z);
+                if (i == 0 || i == NX || j == 0 || j == NY || globalK == 0 || globalK == NZ - 1) {
+                    U[ComputeIndex(i,j,k)] = U_New[ComputeIndex(i,j,k)] = Phi(x, y, z);
                 }
             }
         }
     }
 
     // Буферы обмена границ
-    std::vector <double> aSendUp ((NX + 1)*(NY + 1)), aSendDown ((NX + 1)*(NY + 1));
-    std::vector <double> aRecvUp ((NX + 1)*(NY + 1)), aRecvDown ((NX + 1)*(NY + 1));
+    std::vector <double> aSendUp((NX + 1)*(NY + 1)), aSendDown((NX + 1)*(NY + 1));
+    std::vector <double> aRecvUp((NX + 1)*(NY + 1)), aRecvDown((NX + 1)*(NY + 1));
 
     double aLocalErr = 0, aGlobalErr = 1;
     size_t anIterations = 0;
     double aStart = MPI_Wtime();
 
     for (; aGlobalErr > EPS && anIterations < MAX_ITER; ++anIterations) {
-        // Обмен граничными слоями
         MPI_Request aReqs[4];
-
-        // Если не последний процесс
+        int reqsCount = 0;
+    
+        // Асинхронный обмен
         if (aRank != aSize - 1) {
             for (size_t i = 0; i <= NX; ++i) {
                 for (size_t j = 0; j <= NY; ++j) {
-                    aSendUp[i * (NY + 1) + j] = U[ComputeIndex (i, j, aLocalNZ - 2)];
+                    aSendUp[i * (NY + 1) + j] = U[ComputeIndex(i, j, aLocalNZ - 2)];
                 }
             }
-
-            MPI_Isend (aSendUp.data(), aSendUp.size(), MPI_DOUBLE, aRank + 1, 0, MPI_COMM_WORLD, &aReqs[0]);
-            MPI_Irecv (aRecvUp.data(), aRecvUp.size(), MPI_DOUBLE, aRank + 1, 1, MPI_COMM_WORLD, &aReqs[1]);
+    
+            MPI_Isend (aSendUp.data(), aSendUp.size(), MPI_DOUBLE, aRank + 1, 0, MPI_COMM_WORLD, &aReqs[reqsCount++]);
+            MPI_Irecv (aRecvUp.data(), aRecvUp.size(), MPI_DOUBLE, aRank + 1, 1, MPI_COMM_WORLD, &aReqs[reqsCount++]);
         }
-
-        // Если не первый процесс
+    
         if (aRank != 0) {
             for (size_t i = 0; i <= NX; ++i) {
                 for (size_t j = 0; j <= NY; ++j) {
-                    aSendDown[i * (NY + 1) + j] = U[ComputeIndex (i, j, 1)];
+                    aSendDown[i * (NY + 1) + j] = U[ComputeIndex(i, j, 1)];
                 }
             }
 
-            MPI_Isend (aSendDown.data(), aSendDown.size(), MPI_DOUBLE, aRank - 1, 1, MPI_COMM_WORLD, &aReqs[2]);
-            MPI_Irecv (aRecvDown.data(), aRecvDown.size(), MPI_DOUBLE, aRank - 1, 0, MPI_COMM_WORLD, &aReqs[3]);
+            MPI_Isend (aSendDown.data(), aSendDown.size(), MPI_DOUBLE, aRank - 1, 1, MPI_COMM_WORLD, &aReqs[reqsCount++]);
+            MPI_Irecv (aRecvDown.data(), aRecvDown.size(), MPI_DOUBLE, aRank - 1, 0, MPI_COMM_WORLD, &aReqs[reqsCount++]);
         }
-
-        // Основной расчёт: обновление внутренних точек
+    
+        // Вычисляем внутренние точки, НЕ зависящие от границ
         aLocalErr = 0.0;
         for (size_t i = 1; i < NX; ++i) {
             for (size_t j = 1; j < NY; ++j) {
-                for (size_t k = 1; k < aLocalNZ - 1; ++k) {
-                    // Определяется какой глобальный индекс соответствует текущему локальному индексу k для данного процесса
-                    size_t aGlobalK = (aRank == 0) ? k
-                                    : (aRank == aSize - 1) ? NZ - aLocalNZ + k + 1
-                                    : k + aRank * (aLocalNZ - 2) - 1;
-
+                for (size_t k = 2; k < aLocalNZ - 2; ++k) {
+                    size_t globalK = aRank * localNZ + k - 1;
                     double x = MINX + i * Hx;
                     double y = MINY + j * Hy;
-                    double z = MINZ + aGlobalK * Hz;
-
-                    // Считаем по формуле Якоби
+                    double z = MINZ + globalK * Hz;
+    
                     double aPhi = (
                         Cx * (U[ComputeIndex(i+1,j,k)] + U[ComputeIndex(i-1,j,k)]) +
                         Cy * (U[ComputeIndex(i,j+1,k)] + U[ComputeIndex(i,j-1,k)]) +
                         Cz * (U[ComputeIndex(i,j,k+1)] + U[ComputeIndex(i,j,k-1)]) -
                         RightSideFunction (x,y,z)
                     ) / Cc;
-
-                    // Считаем локальную ошибку
+    
                     aLocalErr = std::max (aLocalErr, std::abs (aPhi - U[ComputeIndex(i,j,k)]));
-                    U_New[ComputeIndex(i,j,k)] = aPhi;
+                    U_New[ComputeIndex (i,j,k)] = aPhi;
+                }
+            }
+        }
+    
+        // Дождаться обменов
+        if (reqsCount > 0) {
+            MPI_Waitall (reqsCount, aReqs, MPI_STATUSES_IGNORE);
+        }
+
+        // Записать полученные граничные слои
+        if (aRank != aSize - 1) {
+            for (size_t i = 0; i <= NX; ++i) {
+                for (size_t j = 0; j <= NY; ++j) {
+                    U[ComputeIndex(i, j, aLocalNZ - 1)] = aRecvUp[i * (NY + 1) + j];
                 }
             }
         }
 
-        // Завершение обменов и запись границ
-        if (aRank != aSize - 1) {
-            MPI_Waitall(2, aReqs, MPI_STATUSES_IGNORE);
-            for (size_t i = 1; i < NX; ++i) {
-                for (size_t j = 1; j < NY; ++j) {
-                    U_New[ComputeIndex(i, j, aLocalNZ - 1)] = aRecvUp[i * (NY + 1) + j];
+        if (aRank != 0) {
+            for (size_t i = 0; i <= NX; ++i) {
+                for (size_t j = 0; j <= NY; ++j) {
+                    U[ComputeIndex(i, j, 0)] = aRecvDown[i * (NY + 1) + j];
                 }
             }
         }
-        if (aRank != 0) {
-            MPI_Waitall(2, aReqs + 2, MPI_STATUSES_IGNORE);
-            for (size_t i = 1; i < NX; ++i) {
-                for (size_t j = 1; j < NY; ++j) {
-                    U_New[ComputeIndex(i, j, 0)] = aRecvDown[i * (NY + 1) + j];
+
+        // Теперь считаем ГРАНИЧНЫЕ слои
+        for (size_t i = 1; i < NX; ++i) {
+            for (size_t j = 1; j < NY; ++j) {
+                for (size_t k : {1ul, aLocalNZ - 2}) {
+                    size_t globalK = aRank * localNZ + k - 1;
+                    if (globalK == 0 || globalK == NZ - 1) {
+                        continue;
+                    }
+
+                    double x = MINX + i * Hx;
+                    double y = MINY + j * Hy;
+                    double z = MINZ + globalK * Hz;
+    
+                    double aPhi = (
+                        Cx * (U[ComputeIndex(i+1,j,k)] + U[ComputeIndex(i-1,j,k)]) +
+                        Cy * (U[ComputeIndex(i,j+1,k)] + U[ComputeIndex(i,j-1,k)]) +
+                        Cz * (U[ComputeIndex(i,j,k+1)] + U[ComputeIndex(i,j,k-1)]) -
+                        RightSideFunction (x,y,z)
+                    ) / Cc;
+    
+                    aLocalErr = std::max (aLocalErr, std::abs (aPhi - U[ComputeIndex(i,j,k)]));
+                    U_New[ComputeIndex (i,j,k)] = aPhi;
                 }
             }
         }
@@ -174,7 +198,7 @@ int main (int argc, char** argv) {
         // Обмен указателей (U_New -> U, U_New заполнится на следующей итерации)
         std::swap (U, U_New);
 
-        MPI_Allreduce(&aLocalErr, &aGlobalErr, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce (&aLocalErr, &aGlobalErr, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
     }
 
     double anEnd = MPI_Wtime();
